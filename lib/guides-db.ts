4599ac040e7bdf758d1c2adb6e1eadf541c7445e
Kaung31 <159174469+Kaung31@users.noreply.github.com>
2026-02-09 23:24:26 +0000

    Update guides-db.ts

--- lib/guides-db.ts
+++ lib/guides-db.ts
@@ -31,7 +31,6 @@ export const guidesData = {
         title: "Remove Display Screen",
         desc: "Put the flathead screwdriver between screen and stem and take off screen.",
         tools: "Flathead screwdriver",
-        image: "placeholder"
       },
       {
         order: 2,
@@ -39,7 +38,6 @@ export const guidesData = {
         desc: "There are five cables on display, unplug all of them. Throttle and brake lever cables are with heat shrink. Cut heat shrink with cutter.",
         tools: "Heat shrink cutter",
         warning: "Wear gloves to protect your hands from sharp edges.",
-        image: "placeholder"
       },
       {
         order: 3,
@@ -47,14 +45,12 @@ export const guidesData = {
         desc: "Pull out the display from the stem, then put new display in.",
         tip: "There is a grommet at the bottom of the stem - it is needed to put it back properly.",
         parts: "Display screen",
-        image: "placeholder"
       },
       {
         order: 4,
         title: "Connect All Cables",
         desc: "All plugs are different types - if it's not correct, you cannot plug them. Plug them all in.",
         tip: "There are two indicator plugs that are the same type, but red color is RIGHT and white is LEFT.",
-        image: "placeholder"
       },
       {
         order: 5,
@@ -62,7 +58,6 @@ export const guidesData = {
         desc: "Throttle and brake plugs need heat shrink to protect from water.",
         tools: "Heat gun",
         parts: "Heat shrink",
-        image: "placeholder"
       },
       {
         order: 6,
@@ -70,7 +65,6 @@ export const guidesData = {
         desc: "Put a waterproof rubber band on display screen and put it back into the stem.",
         parts: "Rubber band",
         warning: "Don't forget the heat shrink and gasket rubber bands - common mistake!",
-        image: "placeholder"
       }
     ]
   },
@@ -102,21 +96,18 @@ export const guidesData = {
         title: "Open Display Screen",
         desc: "Use small flathead screwdriver to open the display screen cover.",
         tools: "Small flathead",
-        image: "placeholder"
       },
       {
         order: 2,
         title: "Separate Cables from Grommet",
         desc: "Separate the throttle and right indicator cable from the cable organizing grommet.",
         tip: "Usually the far right two cables.",
-        image: "placeholder"
       },
       {
         order: 3,
         title: "Remove Indicator Cover",
         desc: "Remove the indicator cover using flathead screwdriver.",
         tools: "Flathead",
-        image: "placeholder"
       },
       {
         order: 4,
@@ -124,21 +115,18 @@ export const guidesData = {
         desc: "Unbolt 3 bolts and pull out the cable.",
         tools: "Hex 2mm",
         tip: "Keep the bolts safe - you'll need them for reassembly.",
-        image: "placeholder"
       },
       {
         order: 5,
         title: "Remove Hand Grip",
         desc: "Pull it out from the handlebar.",
         tip: "May need to twist while pulling if it's tight.",
-        image: "placeholder"
       },
       {
         order: 6,
         title: "Remove Old Throttle",
         desc: "Just loosen the bolt to remove the throttle.",
         tools: "Hex 3mm",
-        image: "placeholder"
       },
       {
         order: 7,
@@ -147,27 +135,23 @@ export const guidesData = {
         tools: "Small hook, Hex 3mm",
         warning: "This is the tricky part - use the hook to grab the cable loop inside the stem.",
         parts: "New throttle (Metal core)",
-        image: "placeholder"
       },
       {
         order: 8,
         title: "Reinstall Hand Grip",
         desc: "Put the handgrip back onto the handlebar.",
-        image: "placeholder"
       },
       {
         order: 9,
         title: "Reinstall Right Indicator",
         desc: "Put the cable into the big hole, take the cable out by using the hook tool.",
         tools: "Small hook",
-        image: "placeholder"
       },
       {
         order: 10,
         title: "Replace Indicator Cover",
         desc: "Put the indicator cover back.",
         warning: "Do not cover the reflection line on indicator.",
-        image: "placeholder"
       },
       {
         order: 11,
@@ -175,7 +159,6 @@ export const guidesData = {
         desc: "Put the indicator and throttle cable back into the cable organizing grommet.",
         tip: "Indicator cable should be far right and throttle cable next to it.",
         warning: "COMMON MISTAKE: Don't forget to tighten the throttle screw back and put the indicator first before handgrip!",
-        image: "placeholder"
       }
     ]
   },
@@ -238,7 +221,6 @@ export const guidesData = {
         title: "Release Chassis Points",
         desc: "Remove two hex chassis bolts from the back of the battery box.",
         tools: "Hex 2.5",
-        image: "placeholder"
       },
       {
         order: 6,
@@ -285,7 +267,6 @@ export const guidesData = {
         title: "Refit Grommets",
         desc: "Refit the chassis grommet cover.",
         tools: "Hex 2.5",
-        image: "placeholder"
       },
       {
         order: 13,
